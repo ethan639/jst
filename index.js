893c22e20c1285c0d0569f883ab7b8ef36717beb
ethan639 <139851202+ethan639@users.noreply.github.com>
2025-03-30 18:30:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -997,7 +997,7 @@ const ed = 'RUR0dW5uZWw=';
  * @returns {string} Configuration HTML
  */
 function getConfig(userIDs, hostName, proxyIP) {
-	const commonUrlPart = `?encryption=none&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#Wg`;
+	const commonUrlPart = `?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#Wg`;
 
 	// Split the userIDs into an array
 	const userIDArray = userIDs.split(",");
