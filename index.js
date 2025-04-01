3332513dc50300975eade862d523ad12279f29db
ethan639 <139851202+ethan639@users.noreply.github.com>
2025-04-01 08:10:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ import { connect } from 'cloudflare:sockets';
  * User configuration and settings
  * Generate UUID: [Windows] Press "Win + R", input cmd and run: Powershell -NoExit -Command "[guid]::NewGuid()"
  */
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '56552fe3-dce7-44f4-9d89-e375eaffc9ae';
 
 /**
  * Array of proxy server addresses with ports
  * Format: ['hostname:port', 'hostname:port']
  */
-const proxyIPs = ['cdn.xn--b6gac.eu.org:443', 'cdn-all.xn--b6gac.eu.org:443'];
+const proxyIPs = ['ts.hpc.tw:443', 'bestproxy.onecf.eu.org:443'];
 
 // Randomly select a proxy server from the pool
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
